bf64b5bb7a988c193fec960c3c6dd9cdb634b82c
Andrei BRABU <andu.brabu@gmail.com>
2025-03-23 19:31:20 +0100

    dataloader

---
+++ data_loader.py
@@ -0,0 +1,50 @@
+import glob
+import pandas as pd
+import json
+
+class DataLoaderECHR:
+    def __init__(self, train_path:str, test_path:str, val_path:str):
+        json_files_train = glob.glob(train_path+'/*.json')
+        json_files_dev = glob.glob(val_path + '/*.json')
+        json_files_test = glob.glob(test_path+'/*.json')
+        
+        df_train = self._concatenate_files_in_df(json_files_train)
+        df_test = self._concatenate_files_in_df(json_files_test)
+        df_val = self._concatenate_files_in_df(json_files_dev)
+        
+        df_train = self._add_binary_violation(df_train)
+        df_test = self._add_binary_violation(df_test)
+        df_val = self._add_binary_violation(df_val)
+        df_train_dev = pd.concat([df_train,df_val], ignore_index=True)
+        
+        return {'train':df_train, 'test': df_test, 'val': df_val, 'test_dev': df_train_dev}
+
+
+    def _concatenate_files_in_df(self, json_files)->pd.DataFrame:
+        rows = []
+
+        for file in json_files:
+            with open(file, 'r') as f:
+                data  = json.load(f)
+                # If needed, process certain fields (e.g., join lists) here:
+                # data['TEXT'] = ' '.join(data['TEXT'])
+                rows.append(data)
+
+        # Create the global DataFrame
+        df = pd.DataFrame(rows)
+        return df
+    
+    def _add_binary_violation(self, df):
+        columns_to_check = [
+            'VIOLATED_ARTICLES', 'VIOLATED_PARAGRAPHS', 'VIOLATED_BULLETPOINTS'
+        ]
+
+        # Create the new binary column
+        df['binary violates'] = df[columns_to_check].apply(
+            lambda row: 1 if any(len(item) > 0 for item in row) else 0,
+            axis=1
+        )
+        
+        return df
+
+
